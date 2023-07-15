468e46413163bdb29e3b1c5cc396d884f19f4c0f
Riteshk64 <riteshkumar.hb@gmail.com>
2023-07-15 17:48:30 +0530

    displayed shows and actors data.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import { searchForShows, searchForActors } from './../api/tvmaze';
 import SearchForm from '../components/SearchFrom';
+import ShowGrid from '../components/Shows/ShowGrid';
+import ActorGrid from '../components/Actors/ActorGrid';
 
 const Home = () => {
   // results from api
@@ -36,12 +38,16 @@ const Home = () => {
       return <div>Error occured: {apiDataError.message}</div>;
     }
 
+    if (apiData?.length === 0) {
+      return <div>No results</div>;
+    }
+
     if (apiData) {
-      return apiData[0].show
-        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
-        : apiData.map(data => (
-            <div key={data.person.id}>{data.person.name}</div>
-          ));
+      return apiData[0].show ? (
+        <ShowGrid shows={apiData} />
+      ) : (
+        <ActorGrid actors={apiData} />
+      );
     }
 
     return null;
