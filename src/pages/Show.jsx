96dea4a3f0ddd5356b5b74c3e96b2163678aeb29
Riteshk64 <riteshkumar.hb@gmail.com>
2023-07-20 19:18:04 +0530

    Fetch show data in show component.

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,8 +1,34 @@
 // To display individual show pages with complete info
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getShowById } from '../api/tvmaze';
 const Show = () => {
-  const { showId } = useParams();
-  return <div>Show id is {showId}</div>;
+  const { showId } = useParams(); // useParams() - To access parameters in the current route(url).
+  const [showData, setShowData] = useState(null); // For data from show id
+  const [showError, setShowError] = useState(null); // For errors
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getShowById(showId);
+        setShowData(data);
+      } catch (err) {
+        setShowError(err);
+      }
+    }
+
+    fetchData();
+  }, [showId]); // The function code runs when the component runs and the return statement runs the component unmounts. Used the manipulate the page during the component's life cycle.
+
+  if (showError) {
+    return <div>We have an error: {showError.message}</div>;
+  }
+
+  if (showData) {
+    return <div>Got show data: {showData.name}</div>;
+  }
+
+  return <div>Data is loading...</div>;
 };
 
 export default Show;
